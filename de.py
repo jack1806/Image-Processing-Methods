24fe0029f8772cc319cac9dbfb5cd47e9bfdc3fa
jack1806 <yashtodkar2@gmail.com>
2017-10-31 11:19:00 +0530

    Photos Updated

--- de.py
+++ de.py
@@ -1,19 +1,14 @@
 import sys
-
-sys.path.append('/usr/local/lib/python3.5/dist-packages')
-
 import cv2
 import numpy as np
 
-img = cv2.imread('/home/satyak/Desktop/Image/de.png', 0)
+img = cv2.imread("second.jpg", 0)
 
 kernel = np.ones((5,5), np.uint8)
 
 img_erosion = cv2.erode(img, kernel, iterations=1)
 img_dilation = cv2.dilate(img, kernel, iterations=1)
 
-cv2.imshow('Input', img)
-cv2.imshow('Erosion', img_erosion)
-cv2.imshow('Dilation', img_dilation)
+cv2.imwrite("dilation.jpg",img_dilation)
+cv2.imwrite("erosion.jpg",img_erosion)
 
-cv2.waitKey(0)
--- segmentation.py
+++ segmentation.py
@@ -1,14 +1,13 @@
 import sys
-
-sys.path.append('/usr/local/lib/python3.5/dist-packages')
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
-img = cv2.imread('/home/satyak/Desktop/Image/coins.jpg')
+img = cv2.imread('first.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
 # noise removal
+
 kernel = np.ones((3,3),np.uint8)
 opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
 
@@ -18,9 +17,6 @@ sure_bg = cv2.dilate(opening,kernel,iterations=3)
 # Finding sure foreground area
 dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
 ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+cv2.imwrite("segment.jpg",sure_fg)
 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(sure_fg),plt.title('After segmentation')
-plt.xticks([]), plt.yticks([])
-plt.show()
+
